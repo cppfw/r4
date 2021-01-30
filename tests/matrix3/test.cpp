613d8bb69499dec9156a20afd99ae681b09b7eb6
Ivan Gagis <igagis@gmail.com>
2021-01-30 23:42:11 +0200

    separating tests from main()

---
+++ tests/matrix3/test.cpp
@@ -0,0 +1,429 @@
+#include <utki/debug.hpp>
+#include <utki/math.hpp>
+
+#include "../../src/r4/matrix.hpp"
+
+#include <sstream>
+
+void test(){
+	// test operator<<
+    {
+        r4::matrix3<int> m;
+		m.set_identity();
+
+		std::stringstream ss;
+
+		ss << m;
+
+		auto cmp =
+                "|1 0 0" "\n"
+                "|0 1 0" "\n"
+                "|0 0 1" "\n";
+
+		// TRACE_ALWAYS(<< "m = " << ss.str() << std::endl)
+		// TRACE_ALWAYS(<< "cmp = " << cmp << std::endl)
+
+		auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+        // v.negate();
+
+        // ASSERT_ALWAYS(!v.is_negative())
+        // ASSERT_ALWAYS(v.x == -13)
+        // ASSERT_ALWAYS(v.y == 14)
+    }
+
+    // test operator[]
+    {
+        r4::matrix3<int> m;
+
+        m[0][0] = 1;
+        m[0][1] = 2;
+        m[0][2] = 3;
+        m[1][0] = 4;
+        m[1][1] = 5;
+        m[1][2] = 6;
+        m[2][0] = 7;
+        m[2][1] = 8;
+        m[2][2] = 9;
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|1 2 3" "\n"
+                "|4 5 6" "\n"
+                "|7 8 9" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test constructor from vector4 arguments
+    {
+        r4::vector4<int> r0(1, 2, 3, 4);
+        r4::vector4<int> r1(5, 6, 7, 8);
+        r4::vector4<int> r2(9, 10, 11, 12);
+
+        r4::matrix3<int> m(r0, r1, r2);
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|1 2 3" "\n"
+                "|5 6 7" "\n"
+                "|9 10 11" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test element type conversion
+    {
+        r4::matrix3<float> mf{
+            { 1.1f, 2.2f, 3.3f },
+            { 4.4f, 5.5f, 6.6f },
+            { 7.7f, 8.8f, 9.9f }
+        };
+
+        auto mi = mf.to<int>();
+
+        std::stringstream ss;
+
+        ss << mi;
+
+        auto cmp =
+                "|1 2 3" "\n"
+                "|4 5 6" "\n"
+                "|7 8 9" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test operator*(vector2)
+    {
+        r4::vector2<int> v(2, 3);
+
+        r4::matrix3<int> m{
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 8, 9, 10 }
+            };
+        
+        auto r = m * v;
+
+        ASSERT_INFO_ALWAYS(r[0] == 13, "r[0] = " << r[0])
+        ASSERT_INFO_ALWAYS(r[1] == 28, "r[1] = " << r[1])
+    }
+
+    // test operator*(vector3)
+    {
+        r4::vector3<int> v(2, 3, 4);
+
+        r4::matrix3<int> m{
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 8, 9, 10 }
+            };
+        
+        auto r = m * v;
+
+        ASSERT_INFO_ALWAYS(r[0] == 2 * 2 + 3 * 3 + 4 * 4, "r[0] = " << r[0])
+        ASSERT_INFO_ALWAYS(r[1] == 2 * 5 + 3 * 6 + 4 * 7, "r[1] = " << r[1])
+        ASSERT_INFO_ALWAYS(r[2] == 2 * 8 + 3 * 9 + 4 * 10, "r[2] = " << r[2])
+    }
+
+    // test transpose()
+    {
+        r4::matrix3<int> m{
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        m.transpose();
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|1 4 7" "\n"
+                "|2 5 8" "\n"
+                "|3 6 9" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test operator*(matrix3)
+    {
+        r4::matrix3<int> m1{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+        r4::matrix3<int> m2{
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 8, 9, 10 }
+            };
+
+        auto r = m1 * m2;
+
+        ASSERT_INFO_ALWAYS(r[0][0] == 1 * 2 + 2 * 5 + 3 * 8, "r[0][0] = " << r[0][0])
+        ASSERT_INFO_ALWAYS(r[1][0] == 4 * 2 + 5 * 5 + 6 * 8, "r[1][0] = " << r[1][0])
+        ASSERT_INFO_ALWAYS(r[2][0] == 7 * 2 + 8 * 5 + 9 * 8, "r[2][0] = " << r[2][0])
+
+        ASSERT_INFO_ALWAYS(r[0][1] == 1 * 3 + 2 * 6 + 3 * 9, "r[0][1] = " << r[0][1])
+        ASSERT_INFO_ALWAYS(r[1][1] == 4 * 3 + 5 * 6 + 6 * 9, "r[1][1] = " << r[1][1])
+        ASSERT_INFO_ALWAYS(r[2][1] == 7 * 3 + 8 * 6 + 9 * 9, "r[2][1] = " << r[2][1])
+
+        ASSERT_INFO_ALWAYS(r[0][2] == 1 * 4 + 2 * 7 + 3 * 10, "r[0][2] = " << r[0][2])
+        ASSERT_INFO_ALWAYS(r[1][2] == 4 * 4 + 5 * 7 + 6 * 10, "r[1][2] = " << r[1][2])
+        ASSERT_INFO_ALWAYS(r[2][2] == 7 * 4 + 8 * 7 + 9 * 10, "r[2][2] = " << r[2][2])
+    }
+
+    // test operator*=(matrix3)
+    {
+        r4::matrix3<int> m1{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+        r4::matrix3<int> m2{
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 8, 9, 10 }
+            };
+        
+        auto r = m1 * m2;
+
+        m1 *= m2;
+
+        ASSERT_INFO_ALWAYS(m1 == r, "m1 = " << m1 << " r = " << r)
+    }
+
+    // test left_mul(matrix3)
+    {
+        r4::matrix3<int> m1{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+        r4::matrix3<int> m2{
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 8, 9, 10 }
+            };
+        
+        auto r = m1 * m2;
+
+        m2.left_mul(m1);
+
+        ASSERT_INFO_ALWAYS(m2 == r, "m2 = " << m2 << " r = " << r)
+    }
+
+    // test scale(x, y, z)
+    {
+        r4::matrix3<int> m{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+        
+        m.scale(2, 3, 4);
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|2 6 12" "\n"
+                "|8 15 24" "\n"
+                "|14 24 36" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test scale(x, y)
+    {
+        r4::matrix3<int> m{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+        
+        m.scale(2, 3);
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|2 6 3" "\n"
+                "|8 15 6" "\n"
+                "|14 24 9" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test scale(s)
+    {
+        r4::matrix3<int> m{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+        
+        m.scale(2);
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|2 4 6" "\n"
+                "|8 10 12" "\n"
+                "|14 16 18" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m = " << str << "\ncmp = " << cmp)
+    }
+
+    // test translate(x, y)
+    {
+        r4::matrix3<int> m{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+        
+        m.translate(2, 3);
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+                "|1 2 11" "\n"
+                "|4 5 29" "\n"
+                "|7 8 47" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m =\n" << str << "\ncmp =\n" << cmp)
+    }
+
+    // test translate(vector2)
+    {
+        r4::matrix3<int> m{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+        
+        m.translate(r4::vector2<int>{2, 3});
+
+        std::stringstream ss;
+
+        ss << m;
+
+        auto cmp =
+            "|1 2 11" "\n"
+            "|4 5 29" "\n"
+            "|7 8 47" "\n";
+        auto str = ss.str();
+
+        ASSERT_INFO_ALWAYS(str == cmp, "m =\n" << str << "\ncmp =\n" << cmp)
+    }
+
+    // test rotate(a)
+    {
+        r4::matrix3<float> m{
+                { 10, 20, 30 },
+                { 40, 50, 60 },
+                { 70, 80, 90 }
+            };
+        
+        float a = utki::pi<float>() / 6;
+
+        decltype(m) res(m);
+        res.rotate(a);
+
+        using std::sin;
+        using std::cos;
+
+        r4::matrix3<float> rot{
+            {cos(a), -sin(a), 0},
+            {sin(a), cos(a), 0},
+            {0, 0, 1}
+        };
+
+        auto cmp = m * rot;
+
+        cmp *= 1000.0f;
+        res *= 1000.0f;
+
+        ASSERT_INFO_ALWAYS(res.to<int>() == cmp.to<int>(), "res = " << res << " cmp = " << cmp)
+    }
+
+	// test minor(r, c)
+	{
+		r4::matrix3<int> m{
+			{3, 0, 2},
+			{2, 0, -2},
+			{0, 1, 1}
+		};
+
+		ASSERT_ALWAYS(m.minor(0, 0) == 2)
+		ASSERT_ALWAYS(m.minor(0, 1) == 2)
+		ASSERT_ALWAYS(m.minor(0, 2) == 2)
+
+		ASSERT_ALWAYS(m.minor(1, 0) == -2)
+		ASSERT_ALWAYS(m.minor(1, 1) == 3)
+		ASSERT_ALWAYS(m.minor(1, 2) == 3)
+
+		ASSERT_ALWAYS(m.minor(2, 0) == 0)
+		ASSERT_ALWAYS(m.minor(2, 1) == -10)
+		ASSERT_ALWAYS(m.minor(2, 2) == 0)
+	}
+
+	// test det()
+	{
+		r4::matrix3<int> m{
+                { 10, 20, 30 },
+                { 40, 50, 60 },
+                { 70, 80, 100 }
+            };
+		
+		ASSERT_ALWAYS(m.det() == -3000)
+	}
+
+	// test inv()
+	{
+		r4::matrix3<float> m{
+		 	{1, 3, 5},
+			{1, 3, 1},
+			{4, 3, 9},
+		};
+
+		auto i = m * m.inv();
+
+		const float epsilon = 1e-6;
+
+		auto diff = decltype(m)().set_identity() - i;
+
+		diff.snap_to_zero(epsilon);
+
+		ASSERT_INFO_ALWAYS(diff == decltype(m)().set(0), std::endl << "i = " << i.snap_to_zero(epsilon) << std::endl << "diff = " << diff)
+	}
+}
---
+++ tests/vector2/test.cpp
@@ -0,0 +1,525 @@
+#include <utki/debug.hpp>
+
+#include "../../src/r4/vector.hpp"
+
+void test(){
+	// test constructor(x, y)
+	{
+		r4::vector2<int> v{2, 3};
+
+		ASSERT_ALWAYS(v[0] == 2)
+		ASSERT_ALWAYS(v[1] == 3)
+	}
+
+	// test constructor(xy)
+	{
+		r4::vector2<int> v(3);
+
+		ASSERT_ALWAYS(v[0] == 3)
+		ASSERT_ALWAYS(v[1] == 3)
+	}
+
+	// test constructor(vector3)
+	{
+		r4::vector3<int> v3{3, 4, 5};
+
+		r4::vector2<int> v2{v3};
+
+		ASSERT_ALWAYS(v2[0] == 3)
+		ASSERT_ALWAYS(v2[1] == 4)
+	}
+
+	// test to()
+	{
+		r4::vector2<float> vf{4.4f, 5.5f};
+
+		auto vi = vf.to<int>();
+
+		ASSERT_ALWAYS(vi[0] == 4)
+		ASSERT_ALWAYS(vi[1] == 5)
+	}
+
+	// test operator=(vector3)
+	{
+		r4::vector3<int> v3{3, 4, 5};
+
+		r4::vector2<int> v2(0);
+
+		v2 = v3;
+
+		ASSERT_ALWAYS(v2[0] == 3)
+		ASSERT_ALWAYS(v2[1] == 4)
+	}
+
+	// test operator+(vector3)
+	{
+		r4::vector3<int> v3{3, 4, 5};
+
+		r4::vector2<int> v2{1, 2};
+
+		auto r = v2 + v3;
+
+		ASSERT_ALWAYS(r[0] == 4)
+		ASSERT_ALWAYS(r[1] == 6)
+	}
+
+	// test operator+=(vector2)
+	{
+		r4::vector2<int> v{1, 2};
+
+		v += r4::vector2<int>{3, 4};
+
+		ASSERT_ALWAYS(v[0] == 4)
+		ASSERT_ALWAYS(v[1] == 6)
+	}
+
+	// test operator+(vector2)
+	{
+		r4::vector2<int> v2_0{3, 4};
+
+		r4::vector2<int> v2{1, 2};
+
+		auto r = v2 + v2_0;
+
+		ASSERT_ALWAYS(r[0] == 4)
+		ASSERT_ALWAYS(r[1] == 6)
+	}
+
+	// test operator+(n)
+	{
+		r4::vector2<int> v2{3, 4};
+
+		auto r = v2 + 5;
+
+		ASSERT_ALWAYS(r[0] == 8)
+		ASSERT_ALWAYS(r[1] == 9)
+	}
+
+	// test operator-=(vector2)
+	{
+		r4::vector2<int> v2{1, 2};
+
+		v2 -= r4::vector2<int>{3, 5};
+
+		ASSERT_ALWAYS(v2[0] == -2)
+		ASSERT_ALWAYS(v2[1] == -3)
+	}
+
+	// test operator-(vector2)
+	{
+		r4::vector2<int> v2_0{3, 5};
+
+		r4::vector2<int> v2{1, 2};
+
+		auto r = v2 - v2_0;
+
+		ASSERT_ALWAYS(r[0] == -2)
+		ASSERT_ALWAYS(r[1] == -3)
+	}
+
+	// test operator-(vector3)
+	{
+		r4::vector3<int> v3{3, 5, 6};
+
+		r4::vector2<int> v2{1, 2};
+
+		auto r = v2 - v3;
+
+		ASSERT_ALWAYS(r[0] == -2)
+		ASSERT_ALWAYS(r[1] == -3)
+	}
+
+	// test operator-()
+	{
+		auto r = -r4::vector2<int>{1, 2};
+
+		ASSERT_ALWAYS(r[0] == -1)
+		ASSERT_ALWAYS(r[1] == -2)
+	}
+
+	// test operator*=(n)
+	{
+		r4::vector2<int> v2{2, 3};
+
+		v2 *= 3;
+
+		ASSERT_ALWAYS(v2[0] == 6)
+		ASSERT_ALWAYS(v2[1] == 9)
+	}
+
+	// test operator*(n)
+	{
+		r4::vector2<int> v2{2, 3};
+
+		auto r = v2 * 3;
+
+		ASSERT_ALWAYS(r[0] == 6)
+		ASSERT_ALWAYS(r[1] == 9)
+	}
+
+	// test operator*(n, vector2)
+	{
+		r4::vector2<int> v2{2, 3};
+
+		auto r = 3 * v2;
+
+		ASSERT_ALWAYS(r[0] == 6)
+		ASSERT_ALWAYS(r[1] == 9)
+	}
+
+	// test min(vector2, vector2)
+	{
+		r4::vector2<int> a{2, 3};
+		r4::vector2<int> b{5, 1};
+
+		auto r = min(a, b);
+
+		ASSERT_ALWAYS(r[0] == 2)
+		ASSERT_ALWAYS(r[1] == 1)
+	}
+
+	// test max(vector2, vector2)
+	{
+		r4::vector2<int> a{2, 3};
+		r4::vector2<int> b{5, 1};
+
+		auto r = max(a, b);
+
+		ASSERT_ALWAYS(r[0] == 5)
+		ASSERT_ALWAYS(r[1] == 3)
+	}
+
+	// test operator/=(n)
+	{
+		r4::vector2<int> v2{6, 9};
+
+		v2 /= 3;
+
+		ASSERT_ALWAYS(v2[0] == 2)
+		ASSERT_ALWAYS(v2[1] == 3)
+	}
+
+	// test operator/(n)
+	{
+		r4::vector2<int> v2{6, 9};
+
+		auto r = v2 / 3;
+
+		ASSERT_ALWAYS(r[0] == 2)
+		ASSERT_ALWAYS(r[1] == 3)
+	}
+
+	// test operator*(vector2)
+	{
+		r4::vector2<int> a{3, 4};
+		r4::vector2<int> b{5, 6};
+
+		auto r = a * b;
+
+		ASSERT_ALWAYS(r == 3 * 5 + 4 * 6)
+	}
+
+	// test comp_mul(vector2)
+	{
+		r4::vector2<int> a{3, 4};
+		r4::vector2<int> b{5, 6};
+
+		auto r = a.comp_mul(b);
+
+		ASSERT_ALWAYS(r[0] == 15)
+		ASSERT_ALWAYS(r[1] == 24)
+	}
+
+	// test comp_multiply(vector2)
+	{
+		r4::vector2<int> a{3, 4};
+
+		a.comp_multiply(r4::vector2<int>{5, 6});
+
+		ASSERT_ALWAYS(a[0] == 15)
+		ASSERT_ALWAYS(a[1] == 24)
+	}
+
+	// test comp_div(vector2)
+	{
+		r4::vector2<int> a{6, 15};
+		r4::vector2<int> b{3, 5};
+
+		auto r = a.comp_div(b);
+
+		ASSERT_ALWAYS(r[0] == 2)
+		ASSERT_ALWAYS(r[1] == 3)
+	}
+
+	// test comp_divide(vector2)
+	{
+		r4::vector2<int> a{6, 15};
+
+		a.comp_divide(r4::vector2<int>{3, 5});
+
+		ASSERT_ALWAYS(a[0] == 2)
+		ASSERT_ALWAYS(a[1] == 3)
+	}
+
+	// test is_zero()
+	{
+		{
+			r4::vector2<int> a{0, 0};
+			ASSERT_ALWAYS(a.is_zero())
+		}
+		{
+			r4::vector2<int> a{0, 3};
+			ASSERT_ALWAYS(!a.is_zero())
+		}
+		{
+			r4::vector2<int> a{6, 0};
+			ASSERT_ALWAYS(!a.is_zero())
+		}
+		{
+			r4::vector2<int> a{4, 3};
+			ASSERT_ALWAYS(!a.is_zero())
+		}
+	}
+
+	// test is_positive_or_zero()
+	{
+		{
+			r4::vector2<int> a{0, 0};
+			ASSERT_ALWAYS(a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{0, 3};
+			ASSERT_ALWAYS(a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{6, 0};
+			ASSERT_ALWAYS(a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{4, 3};
+			ASSERT_ALWAYS(a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{0, -3};
+			ASSERT_ALWAYS(!a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{-6, 0};
+			ASSERT_ALWAYS(!a.is_positive_or_zero())
+		}
+		{
+			r4::vector2<int> a{-4, -3};
+			ASSERT_ALWAYS(!a.is_positive_or_zero())
+		}
+	}
+
+	// test is_positive()
+	{
+		{
+			r4::vector2<int> a{0, 0};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+		{
+			r4::vector2<int> a{0, 3};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+		{
+			r4::vector2<int> a{6, 0};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+		{
+			r4::vector2<int> a{4, 3};
+			ASSERT_ALWAYS(a.is_positive())
+		}
+		{
+			r4::vector2<int> a{0, -3};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+		{
+			r4::vector2<int> a{-6, 0};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+		{
+			r4::vector2<int> a{-4, -3};
+			ASSERT_ALWAYS(!a.is_positive())
+		}
+	}
+
+	// test is_negative()
+	{
+		{
+			r4::vector2<int> a{0, 0};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{0, 3};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{6, 0};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{4, 3};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{0, -3};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{-6, 0};
+			ASSERT_ALWAYS(!a.is_negative())
+		}
+		{
+			r4::vector2<int> a{-4, -3};
+			ASSERT_ALWAYS(a.is_negative())
+		}
+	}
+
+    // test negate
+    {
+        r4::vector2<int> v{13, -14};
+        ASSERT_ALWAYS(!v.is_negative())
+
+        v.negate();
+
+        ASSERT_ALWAYS(!v.is_negative())
+        ASSERT_ALWAYS(v.x() == -13)
+        ASSERT_ALWAYS(v.y() == 14)
+    }
+
+	// test abs()
+	{
+		r4::vector2<int> v{-13, -14};
+
+		using std::abs;
+
+		ASSERT_ALWAYS(!v.is_positive_or_zero())
+		ASSERT_ALWAYS(abs(v).is_positive_or_zero())
+	}
+
+	// test norm_pow2()
+	{
+		r4::vector2<int> v{3, 4};
+
+		ASSERT_ALWAYS(v.norm_pow2() == 25)
+	}
+
+	// test norm()
+	{
+		r4::vector2<float> v{3.0f, 4.0f};
+
+		ASSERT_ALWAYS(v.norm() == 5.0f)
+	}
+
+	// test normalize()
+	{
+		r4::vector2<float> v{3.0f, 4.0f};
+
+		v.normalize();
+
+		ASSERT_INFO_ALWAYS(v[0] == 0.6f, "v = " << v)
+		ASSERT_INFO_ALWAYS(v[1] == 0.8f, "v = " << v)
+	}
+
+	// test normed()
+	{
+		r4::vector2<float> v{3.0f, 4.0f};
+
+		auto r = v.normed();
+
+		ASSERT_INFO_ALWAYS(r[0] == 0.6f, "r = " << r)
+		ASSERT_INFO_ALWAYS(r[1] == 0.8f, "r = " << r)
+	}
+
+	// test set(n)
+	{
+		r4::vector2<int> v{3, 4};
+
+		v.set(5);
+
+		ASSERT_ALWAYS(v[0] == 5)
+		ASSERT_ALWAYS(v[1] == v[0])
+	}
+
+	// test set(x, y)
+	{
+		r4::vector2<int> v{3, 4};
+
+		v.set(5, 6);
+
+		ASSERT_ALWAYS(v[0] == 5)
+		ASSERT_ALWAYS(v[1] == 6)
+	}
+
+	// test rotate(a)
+	{
+		r4::vector2<float> v{41.0f, 30.0f};
+
+		v.rotate(utki::pi<float>() / 6);
+
+		auto r = v.to<int>();
+
+		ASSERT_INFO_ALWAYS(r[0] == 20, "r = " << r)
+		ASSERT_INFO_ALWAYS(r[1] == 46, "r = " << r)
+	}
+
+	// test rotated(a)
+	{
+		auto v = r4::vector2<float>{41.0f, 30.0f}.rot(utki::pi<float>() / 6);
+
+		auto r = v.to<int>();
+
+		ASSERT_INFO_ALWAYS(r[0] == 20, "r = " << r)
+		ASSERT_INFO_ALWAYS(r[1] == 46, "r = " << r)
+	}
+
+	// test round(vector2)
+	{
+		using std::round;
+		auto v = round(r4::vector2<float>{4.8f, 3.3f});
+
+		auto r = v.to<int>();
+
+		ASSERT_ALWAYS(r[0] == 5)
+		ASSERT_ALWAYS(r[1] == 3)
+	}
+
+	// test ceil(vector2)
+	{
+		using std::ceil;
+		auto v = ceil(r4::vector2<float>{4.8f, 3.3f});
+
+		auto r = v.to<int>();
+
+		ASSERT_ALWAYS(r[0] == 5)
+		ASSERT_ALWAYS(r[1] == 4)
+	}
+
+	// test floor(vector2)
+	{
+		using std::floor;
+		auto v = floor(r4::vector2<float>{4.8f, 3.3f});
+
+		auto r = v.to<int>();
+
+		ASSERT_ALWAYS(r[0] == 4)
+		ASSERT_ALWAYS(r[1] == 3)
+	}
+
+	// test that vector::vector(A... a) constructor does is not used to convert shared_ptr to vector.
+	{
+		struct base{};
+		struct derived : base{};
+
+		struct ts{
+			void func(const r4::vector2<float>& v){}
+			void func(std::shared_ptr<const base> p){}
+		} s;
+
+		auto p = std::make_shared<const derived>();
+
+		s.func(p);
+	}
+}
