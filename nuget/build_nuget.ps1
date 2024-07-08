51ed14a417ebd4a1e20538efe7c2405b71f45a63
Ivan Gagis <igagis@gmail.com>
2024-07-08 15:11:25 +0300

    msvs: no v141 build

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -25,10 +25,10 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 # msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Debug /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
 # msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v140_Release /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
 
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
-msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
+# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
+# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
+# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Debug /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
+# msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v141_Release /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
 
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Debug_MD /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v142_Release_MD /p:Platform=x86 /v:minimal /nologo; If(!$?){exit 1}
@@ -49,10 +49,10 @@ msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v143_D
 msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v143_Release_MT /p:Platform=x64 /v:minimal /nologo; If(!$?){exit 1}
 
 Write-Host "running tests..."
-../msvs_solution/v141_Debug/unit_tests.exe          --jobs=2 --junit-out=junit_x86_v141_debug.xml; If(!$?){exit 1}
-../msvs_solution/v141_Release/unit_tests.exe        --jobs=2 --junit-out=junit_x86_v141_release.xml; If(!$?){exit 1}
-../msvs_solution/x64/v141_Debug/unit_tests.exe      --jobs=2 --junit-out=junit_x64_v141_debug.xml; If(!$?){exit 1}
-../msvs_solution/x64/v141_Release/unit_tests.exe    --jobs=2 --junit-out=junit_x64_v141_release.xml; If(!$?){exit 1}
+# ../msvs_solution/v141_Debug/unit_tests.exe          --jobs=2 --junit-out=junit_x86_v141_debug.xml; If(!$?){exit 1}
+# ../msvs_solution/v141_Release/unit_tests.exe        --jobs=2 --junit-out=junit_x86_v141_release.xml; If(!$?){exit 1}
+# ../msvs_solution/x64/v141_Debug/unit_tests.exe      --jobs=2 --junit-out=junit_x64_v141_debug.xml; If(!$?){exit 1}
+# ../msvs_solution/x64/v141_Release/unit_tests.exe    --jobs=2 --junit-out=junit_x64_v141_release.xml; If(!$?){exit 1}
 
     ../msvs_solution/v142_Debug_MD/unit_tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_md.xml; If(!$?){exit 1}
     ../msvs_solution/v142_Debug_MT/unit_tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_mt.xml; If(!$?){exit 1}
